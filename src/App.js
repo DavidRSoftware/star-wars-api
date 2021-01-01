aba5cc827bb69983ebf7e0ea60fba9e17b69764a
David Rosen <david.tzvi.rosen@gmail.com>
2021-01-01 11:34:33 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import searchIcon from "./searchIcon.svg";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
+import ListGroup from "react-bootstrap/ListGroup";
+import "./styles.css";
 
 function App() {
+  const [results, setResults] = useState("");
+  const [resultsDisplay, setResultsDisplay] = useState("");
+  const [searchTerm, updateTerm] = useState("");
+  const [previous, updatePrevious] = useState("");
+  const [next, updateNext] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((res) => {
+        console.log(res);
+        setResults(res.data.results);
+        setResultsDisplay(res.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+        setResultsDisplay("error");
+      });
+  }, []);
+
+  const updateResults = (newTerm) => {
+    updateTerm(newTerm);
+    const newResults = results.filter((value) =>
+      value.name.toLowerCase().includes(newTerm.toLowerCase())
+    );
+    setResultsDisplay(newResults);
+  };
+
+  const getResults = () => {
+    if (resultsDisplay === "error")
+      return (
+        <div className="error">
+          A error has occured. Please check to see if you are connected to the
+          internet and try reloading the page.
+        </div>
+      );
+    else if (resultsDisplay !== "")
+      return resultsDisplay.map((value, index) => {
+        return (
+          <ListGroup.Item key={index} variant="warning">
+            {value.name}
+          </ListGroup.Item>
+        );
+      });
+    else return "Loading...";
+  };
+
+  const renderSearchBar = () => {
+    if (resultsDisplay !== "error") {
+      return (
+        <InputGroup className="mb-4">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="searchIcon">
+              <img src={searchIcon} alt="" />
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            value={searchTerm}
+            onChange={(e) => updateResults(e.target.value)}
+            placeholder="filter characters"
+            aria-label="filter characters"
+            aria-describedby="searchIcon"
+            className="yellow"
+          />
+        </InputGroup>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container data-testid="app-component">
+      <h2 data-testid="heading" className="my-4 title">Star Wars Characters</h2>
+      <Row>
+        <Col>{renderSearchBar()}</Col>
+      </Row>
+      <div className="mb-4" id="results">
+        <ListGroup>{getResults()}</ListGroup>
+      </div>
+    </Container>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+afterEach(cleanup);
+it("renders without crashing", ()=> {
+    const div = document.createElement("div");
+    ReactDOM.render(<App></App>, div);
+}) 
+
+it("renders app component correctly", ()=>{
+  const {getByTestId} = render(<App></App>);
+  const heading = getByTestId("heading")
+  expect(getByTestId("app-component")).toContainElement(heading);
+})
